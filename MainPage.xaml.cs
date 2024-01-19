0c4d4af39ceb95b3325b059efa283e9a5f927d9a
sudarsanMS <sudarsan.m@syncfusion.com>
2024-01-19 12:43:03 +0530

    MauiApp1

---
+++ MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.UserAppTheme = AppTheme.Dark;
+        }
+
+    }
+
+}
